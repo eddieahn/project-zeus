ba439d5503499b6bbc5b67fcd0774758be62e118
eddieahn <eahn@adobe.com>
2025-01-19 18:46:05 -0500

    adding feedback table

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ import streamlit as st
 
 def page_2():
     st.title("Resource Assistant")
+    conn = st.connection('feedback_db',type='sql')
+    feedback = conn.query('select * from feedback')
+    st.dataframe(feedback)    
 pg=st.navigation([st.Page("Classification.py",title="Classification Assistant"),st.Page(page_2,title="Resource Assistant")])
 pg.run()
